3f9f37e2c9bf49b33a264a40d5f47aab1b1a5300
Alisa Liu <alisa.46@gmail.com>
2025-02-07 15:33:00 -0800

    constrained beam search is working

--- test_beam_search.py
+++ test_beam_search.py
@@ -1,123 +1,83 @@
-from typing import Optional
-
 import torch
-import torch.nn.functional as F
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers.generation.utils import (
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    LogitsProcessor,
     LogitsProcessorList,
-    StoppingCriteriaList,
 )
 from olmo.torch_util import seed_all
 
 seed_all(42)
 
-model = AutoModelForCausalLM.from_pretrained("allenai/OLMo-2-1124-7B")
-tokenizer = AutoTokenizer.from_pretrained("allenai/OLMo-2-1124-7B")
+model_name_or_path = "models/hf_models/OLMo2-7B-pts200k-t180k-ctx2995/step95972"
+model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+
+
+class SurfaceFormConstraintLogitsProcessor(LogitsProcessor):
+    """
+    [`LogitsProcessor`] which forces the generated sequence of tokens to decode to a specific surface form.
+
+    Args:
+        constraint (`str`):
+            The surface form that the model should generate
+        eos_token_id (`Union[int, List[int], torch.Tensor]`):
+            The id(s) of the *end-of-sequence* token.
+        device (`str`, *optional*, defaults to `"cpu"`):
+            The device to allocate the tensors.
+    """
+
+    def __init__(self, constraint: str, eos_token_id: int, device: str = "cpu"):
+        self.constraint = constraint
+        self.eos_token_id = eos_token_id
+        self.vocab = tokenizer.get_vocab()
+
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
+        decoded_outputs = tokenizer.batch_decode(input_ids, skip_special_tokens=True)
+        for i, decoded_output in enumerate(decoded_outputs):
+            print(f"Seq {i}: current sequence is {tokenizer.convert_ids_to_tokens(input_ids[i])}")
+
+            # when constraint is satisfied, force the next token to be eos
+            if self.constraint == decoded_output:
+                print("    Done!")
+                mask = torch.zeros_like(scores[i], dtype=torch.bool)
+                mask[self.eos_token_id] = True
+                scores[i][~mask] = float("-inf")
+                continue
+
+            # set logits for all invalid next-tokens to -inf
+            valid_next_token_ids = [
+                token_id
+                for token, token_id in self.vocab.items()
+                if self.constraint.replace(decoded_output, "").startswith(tokenizer.decode(token_id))
+            ]
+            print(f"    Valid next-token choices are {tokenizer.convert_ids_to_tokens(valid_next_token_ids)}")
+            mask = torch.zeros_like(scores[i], dtype=torch.bool)
+            mask[valid_next_token_ids] = True
+            scores[i][~mask] = float("-inf")
+            # divide all scores by 2
+            scores[i] /= 2.0
+        return scores
+
+
 input_text = tokenizer.eos_token
 input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 print(input_ids)
-output_ids = model.generate(input_ids, max_new_tokens=100, do_sample=False)
-print(tokenizer.decode(output_ids[0]))
 
-
-def generate(
-    model,
-    tokenizer,
-    input_ids: Optional[torch.Tensor] = None,
-    constraint: str = None,
-    max_new_tokens: Optional[int] = 100,
-    do_sample: bool = False,
-    logits_processor: Optional[LogitsProcessorList] = None,
-    stopping_criteria: Optional[StoppingCriteriaList] = None,
-    **kwargs,
-):
-    """
-    constraint: the sequence of text that the model should match exactly
-    """
-    input_ids = input_ids.to(input_ids.device)
-    vocab = tokenizer.get_vocab()
-
-    # keep track of which sequences are already finished
-    unfinished_sequences = torch.ones(input_ids.shape[0], dtype=torch.long, device=input_ids.device)
-    eos_token_id_tensor = torch.tensor([tokenizer.eos_token_id]).to(input_ids.device)
-    generation_config, model_kwargs = model._prepare_generation_config(model.generation_config, **kwargs)
-    inputs_tensor, model_input_name, model_kwargs = model._prepare_model_inputs(
-        input_ids, generation_config.bos_token_id, model_kwargs
-    )
-
-    batch_size, cur_len = input_ids.shape
-    unfinished_sequences = torch.ones(batch_size, dtype=torch.long, device=input_ids.device)
-    model_kwargs = model._get_initial_cache_position(input_ids, model_kwargs)
-
-    for step in range(max_new_tokens):
-        inputs = model.prepare_inputs_for_generation(input_ids, **model_kwargs)
-        outputs = model.forward(**inputs, return_dict=True)
-        model_kwargs = model._update_model_kwargs_for_generation(outputs, model_kwargs)
-        next_token_logits = outputs.logits[:, -1, :].clone().float()
-
-        # pre-process logits
-        if logits_processor:
-            next_token_logits = logits_processor(input_ids, next_token_logits)
-
-        # can only consider tokens
-        decoded_outputs = tokenizer.batch_decode(input_ids, skip_special_tokens=True)
-        for i, decoded_output in enumerate(decoded_outputs):
-            assert constraint.startswith(decoded_output), f"{constraint} does not start with {decoded_output}"
-            if constraint == decoded_output:
-                next_token_logits[i][tokenizer.eos_token_id] = float("inf")
-                continue
-
-            valid_next_token_ids = [
-                token_id
-                for token, token_id in vocab.items()
-                if constraint.replace(decoded_output, "").startswith(tokenizer.decode(token_id))
-            ]
-            print(f"Valid choices for next token: {tokenizer.convert_ids_to_tokens(valid_next_token_ids)}")
-            mask = torch.zeros_like(next_token_logits[i], dtype=torch.bool)
-            mask[valid_next_token_ids] = True
-            next_token_logits[i][~mask] = float("-inf")
-
-        # decode
-        if do_sample:
-            probs = F.softmax(next_token_logits, dim=-1)
-            next_tokens = torch.multinomial(probs, num_samples=1).squeeze(1)
-        else:
-            next_tokens = torch.argmax(next_token_logits, dim=-1)
-
-        next_tokens = next_tokens * unfinished_sequences + tokenizer.pad_token_id * (1 - unfinished_sequences)
-
-        # update model inputs for next step
-        input_ids = torch.cat([input_ids, next_tokens[:, None]], dim=-1)
-
-        # stopping criteria
-        if stopping_criteria and stopping_criteria(input_ids, None):
-            break
-
-        # if eos_token was found in one sentence, set sentence to finished
-        unfinished_sequences = unfinished_sequences.mul(
-            next_tokens.tile(eos_token_id_tensor.shape[0], 1).ne(eos_token_id_tensor.unsqueeze(1)).prod(dim=0)
-        )
-
-        # stop when each sentence is finished
-        if unfinished_sequences.max() == 0:
-            break
-
-        del outputs
-
-    return input_ids
-
-
-constraint = (
-    "Lexical analysis is the conversion of a text into meaningful lexical tokens based on a lexical grammar."
-)
-output_ids = generate(
-    model,
-    tokenizer,
+constraint = "Lexical analysis is the conversion of a text into meaningful lexical tokens based on a lexical grammar. Learn about the stages, categories, and examples of lexical tokens, and the difference between lexical analysis and large language models."
+logits_processor = LogitsProcessorList([SurfaceFormConstraintLogitsProcessor(constraint, tokenizer.eos_token_id)])
+model.generation_config.early_stopping = True
+output_ids = model.generate(
     input_ids,
-    constraint=constraint,
+    logits_processor=logits_processor,
+    num_beams=4,
+    num_return_sequences=4,
     max_new_tokens=100,
     do_sample=False,
+    synced_gpus=False,
+    # temperature=10.0,
 )
-print(tokenizer.convert_ids_to_tokens(output_ids[0]))
-# print(tokenizer.decode(output_ids[0]))
+for o_ids in output_ids:
+    print(tokenizer.convert_ids_to_tokens(o_ids))
+
 print(tokenizer.convert_ids_to_tokens(tokenizer.encode(tokenizer.eos_token + constraint)))
