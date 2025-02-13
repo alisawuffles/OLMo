3a1c2c1491933c91422dac9ab6657776d2c26d11
Alisa Liu <alisa.46@gmail.com>
2025-02-12 21:44:31 -0800

    jon's alg

--- eval/eval_corrected_bpb.py
+++ eval/eval_corrected_bpb.py
@@ -1,183 +1,333 @@
-import torch
-import torch.nn.functional as F
+import asyncio
+import heapq
 import json
 import os
-import random
-import pandas as pd
-import click
-from tqdm import tqdm
+import uuid
+from functools import partial, wraps
 from pathlib import Path
-from transformers import LogitsProcessorList
-from olmo.util import ensure_dir
-from eval.util import load_model_and_tokenizer
-from eval.custom_logits_process import SurfaceFormConstraintLogitsProcessor
 
-random.seed(42)
+import click
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn.functional as F
+import tqdm.auto as tqdm
+from vllm import AsyncEngineArgs, AsyncLLMEngine, SamplingParams, TokensPrompt
+
+import eval.dp_tokenization as dpt
+from olmo.util import bytes_to_unicode, ensure_dir
 
 
-def get_corrected_bits_per_byte(model, tokenizer, eval_data, batch_size, max_context_length, constraint_beam_size):
-    total_loss = 0
-    total_corrected_loss = 0
-    total_tokens = 0
-    total_bytes = 0
+def coro(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return asyncio.run(f(*args, **kwargs))
 
-    def get_segmentations(constraint):
-        input_ids = tokenizer(tokenizer.eos_token, return_tensors="pt").input_ids
-        logits_processor = LogitsProcessorList([SurfaceFormConstraintLogitsProcessor(constraint, tokenizer)])
-        model.generation_config.early_stopping = True
-        outputs = model.generate(
-            input_ids,
-            logits_processor=logits_processor,
-            num_beams=constraint_beam_size,
-            num_return_sequences=constraint_beam_size,
-            max_new_tokens=model.config.max_position_embeddings,
-            do_sample=False,
-            output_scores=True,
-            return_dict_in_generate=True,
+    return wrapper
+
+
+class CaptureAndSelectLogitsProcessor:
+    def __init__(self, selections):
+        self.selections = selections
+        self.captured_logprobs = {}
+
+    def __call__(self, past, logits):
+        assert not past
+        logprobs = F.log_softmax(logits, 0)
+        mask = torch.ones_like(logits) * -torch.inf
+        for tid in self.selections:
+            mask[tid] = 0
+            self.captured_logprobs[tid] = logprobs[tid].cpu().item()
+
+        return logits + mask
+
+
+class ByteBeamSearch:
+    def __init__(self):
+        self.btu = bytes_to_unicode()
+        self.utb = {v: k for k, v in self.btu.items()}
+
+    @classmethod
+    async def create(cls, llm):
+        self = cls()
+        self.llm = llm
+        self.tokenizer = await llm.get_tokenizer()
+        self.vocab = {bytes(self.utb[c] for c in tok): tid for tok, tid in self.tokenizer.vocab.items()}
+        self.vrev = {tid: tok for tok, tid in self.vocab.items()}
+        self.vtrie = dpt.build_trie(self.vocab.keys())
+        self.rtrie = dpt.build_trie(map(reversed, self.vocab.keys()))
+        for tok, tid in self.vocab.items():
+            pointer = self.vtrie
+            for b in tok:
+                pointer = pointer[b]
+            pointer[None] = tid
+
+            rpointer = self.rtrie
+            for b in reversed(tok):
+                rpointer = rpointer[b]
+            rpointer[None] = tid
+
+        return self
+
+    async def generate(self, *args, **kwargs):
+        request_id = uuid.uuid4()
+        outputs = self.llm.generate(*args, **kwargs, request_id=request_id)
+        async for output in outputs:
+            final_output = output
+        return final_output
+
+    async def get_logprobs(self, tokens, selections):
+        cslp = CaptureAndSelectLogitsProcessor(selections)
+        result = await self.generate(
+            TokensPrompt(prompt_token_ids=tokens),
+            SamplingParams(
+                max_tokens=1,
+                temperature=0,
+                logits_processors=[cslp],
+            ),
         )
-        return outputs
+        return result, cslp.captured_logprobs
 
-    examples = []
+    async def reference_score(self, tokens, add_leading_eos=True, add_trailing_eos=True):
+        lps = (
+            await self.generate(
+                TokensPrompt(
+                    prompt_token_ids=([self.tokenizer.eos_token_id] if add_leading_eos else [])
+                    + tokens
+                    + ([self.tokenizer.eos_token_id] if add_trailing_eos else [])
+                ),
+                SamplingParams(prompt_logprobs=0, max_tokens=1),
+            )
+        ).prompt_logprobs
+        return sum(next(iter(lp.values())).logprob for lp in lps if lp is not None)
 
-    # one document at a time
-    for text in tqdm(eval_data):
-        inputs = tokenizer(
-            tokenizer.eos_token + text + tokenizer.eos_token,
-            return_tensors="pt",
-            padding=True,
-            truncation=True,
-            max_length=max_context_length,
-        )
-        input_ids = inputs.input_ids.to(model.device)
+    async def byte_beam_search(self, text: bytes, beam_width=3, progress=False, score_eos=True):
+        assert isinstance(text, bytes)
+        eos = self.tokenizer.eos_token_id
+        table = [[[0.0, eos, None, None]]]
 
-        with torch.no_grad():
-            # 1. calculate loss of BPE segmentation
-            per_token_loss = model(input_ids=input_ids, labels=input_ids, return_dict=True).loss
-            bpe_loss = (per_token_loss * (input_ids.size(1) - 1)).item()
+        def get_prompt(beam):
+            # walk the beam backwards to get the prompt
+            bpointer, prompt_rev = beam, []
+            while True:
+                prompt_rev.append(bpointer[1])
+                if bpointer[2] is None:
+                    break
+                bpointer = table[bpointer[2][0]][bpointer[2][1]]
 
-            # 2. calculate loss of other segmentations
-            segmentations = get_segmentations(constraint=tokenizer.eos_token + text)
-            segmentation_ids = segmentations.sequences.to(input_ids.device)
-            segmentation_scores = segmentations.sequences_scores.to(input_ids.device)
+            return prompt_rev[::-1]
 
-            # exclude segmentations found by beam search that are equivalent to BPE
-            neq_bpe_mask = ~(segmentation_ids[:, : input_ids.size(1)] == input_ids).all(dim=-1)
-            neq_bpe_mask = neq_bpe_mask.to(input_ids.device)
-            segmentation_ids = segmentation_ids[neq_bpe_mask]
-            segmentation_scores = segmentation_scores[neq_bpe_mask]
+        async def fetch_logprobs(j, beam):
+            if beam[3] is None:
+                prompt = get_prompt(beam)
 
-            # loss returned by beam search is at the token level, so we need to multiply by the length
-            segmentation_lens = (segmentation_ids == tokenizer.eos_token_id).nonzero()[:, 1][1::2]
-            segmentation_loss = -torch.mul(segmentation_scores, segmentation_lens)
+                # scan forwards to get valid tokens
+                valid_tokens, pointer = [], self.vtrie
+                for k in range(j, len(text)):
+                    if (pointer := pointer.get(text[k])) is None:
+                        # print(f"break forward scan {k, text[j]}")
+                        break
+                    if (tid := pointer.get(None)) is not None:
+                        valid_tokens.append(tid)
 
-        total_loss += bpe_loss
-        total_tokens += inputs.attention_mask[..., 1:].sum()
-        segmentation_loss = torch.cat(
-            (segmentation_loss, torch.tensor([bpe_loss], device=segmentation_loss.device))
-        )
-        total_corrected_loss += -torch.logsumexp(-torch.tensor(segmentation_loss), dim=0)
+                _, logprobs = await self.get_logprobs(prompt, valid_tokens)
+                beam[3] = logprobs
 
-        total_bytes += sum(
-            len(text) for text in tokenizer.batch_decode(inputs.input_ids, skip_special_tokens=True)
-        )
+            return beam[3]
 
-        examples.append(
-            {
-                "text": text,
-                "bpe_segmentation": tokenizer.convert_ids_to_tokens(input_ids[0]),
-                "bpe_loss": bpe_loss,
-                "other_segmentations": [tokenizer.convert_ids_to_tokens(ids) for ids in segmentation_ids],
-                "segmentation_loss": segmentation_loss[:-1].tolist(),
-            }
-        )
+        R = partial(tqdm.trange, position=tqdm.tqdm._get_free_pos(), leave=False) if progress else range
+        for i in R(1, len(text) + 1):
+            candidates = []
 
-    bits_per_byte = total_loss / torch.log(torch.tensor(2.0)) / total_bytes
-    corrected_bits_per_byte = total_corrected_loss / torch.log(torch.tensor(2.0)) / total_bytes
-    metrics = {
-        "bits_per_byte": bits_per_byte.item(),
-        "corrected_bits_per_byte": corrected_bits_per_byte.item(),
-        "total_bytes": total_bytes,
-        "num_examples": len(eval_data),
-        "max_context_length": max_context_length,
-        "total_tokens": total_tokens.item(),
-    }
+            # scan backward to get valid tokens
+            rpointer = self.rtrie
+            for j in range(i - 1, -1, -1):
+                if (rpointer := rpointer.get(text[j])) is None:
+                    # print(f"break reverse scan {j, text[j]}")
+                    break
+                if (tid := rpointer.get(None)) is not None:
+                    for k, beam in enumerate(table[j]):
+                        logprobs = await fetch_logprobs(j, beam)
+                        if (logprob := logprobs.get(tid)) is not None:
+                            cum_logprob = logprob + beam[0]
+                            candidate = [cum_logprob, tid, (j, k), None]
+                            candidates.append(candidate)
+                        else:
+                            print(f"warning: pruning beam {(j, k)} due to context limit")
 
-    return metrics, examples
+            if not candidates:
+                print("warning: no beams fit in context limit")
+                return
+
+            cur_width = beam_width(i) if callable(beam_width) else beam_width
+            new_beams = heapq.nlargest(cur_width, candidates)
+            table.append(new_beams)
+
+        final_beams, result = table[-1], {}
+        for k, beam in enumerate(final_beams):
+            prompt, score = get_prompt(beam), beam[0]
+
+            # To taste: score terminating EOS
+            if score_eos:
+                _, logprobs = await self.get_logprobs(prompt, [eos])
+                if eos not in logprobs:
+                    print(f"warning: pruning beam {(len(table), k)} due to context limit")
+                    continue
+
+                score += logprobs[eos]
+
+            # To taste: remove the initial EOS token
+            result[tuple(prompt[1:])] = score
+
+        return result
 
 
-"""
-model_name_or_path=models/hf_models/OLMo2-7B-pts200k-t180k-ctx2995
-step=95972
-python -m eval.eval_corrected_bpb \
-    --model_name_or_path $model_name_or_path \
-    --step $step \
-    --output_dir results/bpb/temp \
-    --max_num_examples 1
+def beam_schedule(text_len, beam_min=2, beam_max=1000):
+    lmin, lmax = np.log(beam_min), np.log(beam_max)
 
-EOS is placed at both the beginning and the end of a document.
-"""
+    def inner(i):
+        return int(np.ceil(np.exp(lmin + (lmax - lmin) * (i - 1) / (text_len - 1))))
+
+    return inner
+
+
+async def run_document(BBS, i, text, max_ctx, dest_file, beam_min, beam_max, verbose=False):
+    eos = BBS.tokenizer.eos_token_id
+    tokens = BBS.tokenizer.encode(text)
+    tokens.append(eos)
+    tokens = tokens[:max_ctx]
+    score_eos = False
+    tokens_clipped = tokens
+    if tokens[-1] == eos:
+        score_eos = True
+        tokens_clipped = tokens[:-1]
+
+    text_clipped = BBS.tokenizer.decode(tokens_clipped).encode()
+    ref_loss = -await BBS.reference_score(tokens_clipped)
+    ref_bpb = ref_loss / len(text_clipped) / np.log(2)
+
+    result = await BBS.byte_beam_search(
+        text_clipped,
+        beam_width=beam_schedule(len(text_clipped), beam_min, beam_max),
+        progress=True,
+        score_eos=score_eos,
+    )
+
+    bpe_in_beams = False
+    our_losses = [ref_loss]
+    for token_ids, score in result.items():
+        if token_ids == tokens_clipped:
+            bpe_in_beams = True
+            continue
+        our_losses.append(-score)
+
+    if verbose:
+        print(f"BPE segmentation: {BBS.tokenizer.convert_ids_to_tokens(tokens_clipped)}")
+        print(f"Reference loss: {ref_loss}")
+
+        for token_ids, score in list(result.items())[:5]:
+            print(f"Segmentation: {BBS.tokenizer.convert_ids_to_tokens(token_ids)}")
+            print(f"Loss: {score}")
+
+    corrected_loss = -torch.logsumexp(-torch.tensor(our_losses), 0).item()
+    corrected_bpb = corrected_loss / len(text_clipped) / np.log(2)
+    beams_loss = -torch.logsumexp(torch.tensor(list(result.values())), 0).item()
+    beams_bpb = beams_loss / len(text_clipped) / np.log(2)
+
+    tqdm.tqdm.write(f"{i}: {ref_bpb:.6f}, {corrected_bpb:.6f}, {ref_bpb - corrected_bpb:.6f}")
+    with open(dest_file, "wt") as fout:
+        output = {
+            "bpe": (tokens_clipped, ref_loss),
+            "beams": list(result.items()),
+            "ref_loss": ref_loss,
+            "ref_bpb": ref_bpb,
+            "corrected_loss": corrected_loss,
+            "corrected_bpb": corrected_bpb,
+            "beams_loss": beams_loss,
+            "beams_bpb": beams_bpb,
+            "len": len(text_clipped),
+            "bpe_in_beams": str(bpe_in_beams).lower(),
+        }
+        json.dump(output, fout)
 
 
 @click.command()
-@click.option("--model_name_or_path", type=str, default="OLMo2-7B-npt200k")
-@click.option("--step", type=int, default=None)
-@click.option("--output_dir", type=str, default="results/bpb/olmo-20k")
-@click.option(
-    "--eval_dir",
-    type=str,
-    help="Path to folder with jsonl files",
-    default="olmo_data/olmo2_shuffle/olmo2_shuffle",
-)
-@click.option("--max_num_examples", type=int, default=None)
-@click.option("--eval_batch_size", type=int, default=8)
-@click.option("--max_context_length", type=int, default=None)
-@click.option("--constraint_beam_size", type=int, default=4)
-def main(
+@click.option("--data_dir", default="olmo_data/olmo2_shuffle")
+@click.option("--model_name_or_path", default="models/hf_models/OLMo2-7B-pts200k-t180k-ctx2995/step95972")
+@click.option("--output_dir", default="results/dp_tcs_bpb/OLMo2-7B-pts200k-t180k-ctx2995/step95972")
+@click.option("--max_num_examples", default=1)
+@click.option("--max_context_length", default=128)
+@click.option("--batch_size", default=32)
+@click.option("--beam_min", default=2)
+@click.option("--beam_max", default=100)
+@coro
+async def main(
+    data_dir: str,
     model_name_or_path: str,
-    step: int,
     output_dir: str,
-    eval_dir: str,
     max_num_examples: int,
-    eval_batch_size: int,
     max_context_length: int,
-    constraint_beam_size: int,
+    batch_size: int,
+    beam_min: int,
+    beam_max: int,
 ):
-    model, tokenizer = load_model_and_tokenizer(model_name_or_path, step=step, padding_side="right")
-    max_context_length = max_context_length or model.config.max_position_embeddings
-    print(f"Using max context length of {max_context_length}")
+    data_dir = Path(data_dir)
+    model_dir, output_dir = Path(model_name_or_path), Path(output_dir)
+    ensure_dir(output_dir)
+    ensure_dir(output_dir / "data")
+    llm = AsyncLLMEngine.from_engine_args(
+        AsyncEngineArgs(
+            str(model_dir),
+            enforce_eager=True,
+            disable_log_stats=True,
+            enable_chunked_prefill=True,
+            max_num_batched_tokens=512,
+            enable_prefix_caching=True,
+        ),
+    )
+    llm.log_requests = False
+    BBS = await ByteBeamSearch.create(llm)
 
     # read .jsonl files from eval_dir until we have max_num_examples
-    eval_data = []
-    for eval_file in os.listdir(eval_dir):
-        nrows = max_num_examples - len(eval_data)
-        df = pd.read_json(os.path.join(eval_dir, eval_file), lines=True, compression="zstd", nrows=nrows)
-        eval_data.extend(df.text.tolist())
-        if len(eval_data) >= max_num_examples:
+    data = []
+    for eval_file in os.listdir(data_dir):
+        nrows = max_num_examples - len(data)
+        df = pd.read_json(data_dir / eval_file, lines=True, compression="zstd", nrows=nrows)
+        data.extend(df.text.tolist())
+        if len(data) >= max_num_examples:
             break
 
-    eval_data = random.sample(eval_data, max_num_examples)
-    print(f"Loaded {len(eval_data)} examples.")
+    dltasks = set()
+    for i in range(max_num_examples):
+        dest_file = output_dir / f"data/{i}.json"
+        if os.path.exists(dest_file):
+            continue
+        if len(dltasks) >= batch_size:
+            _done, dltasks = await asyncio.wait(dltasks, return_when=asyncio.FIRST_COMPLETED)
+        dltasks.add(
+            asyncio.create_task(run_document(BBS, i, data[i], max_context_length, dest_file, beam_min, beam_max))
+        )
+    await asyncio.wait(dltasks)
 
-    metrics, examples = get_corrected_bits_per_byte(
-        model,
-        tokenizer,
-        eval_data=eval_data,
-        batch_size=eval_batch_size,
-        max_context_length=max_context_length,
-        constraint_beam_size=constraint_beam_size,
-    )
+    # read results
+    total_loss, total_corrected_loss, total_beams_loss, total_bytes = 0, 0, 0
+    for i in range(max_num_examples):
+        with open(output_dir / f"data/{i}.json", "rt") as fin:
+            result = json.load(fin)
+            total_loss += result["ref_loss"]
+            total_corrected_loss += result["corrected_loss"]
+            total_beams_loss += result["beams_loss"]
+            total_bytes += result["len"]
 
-    # print metrics
-    for k, v in metrics.items():
-        print(f"{k}: {v}")
+    metrics = {
+        "bpb": total_loss / total_bytes / np.log(2),
+        "corrected_bpb": total_corrected_loss / total_bytes / np.log(2),
+        "beams_bpb": total_beams_loss / total_bytes / np.log(2),
+        "total_bytes": total_bytes,
+    }
 
-    output_dir = Path(output_dir)
-    ensure_dir(output_dir)
-    print(f"Saving results to {output_dir}")
-
-    with open(output_dir / "metrics.json", "w") as fo:
-        json.dump(metrics, fo, indent=4)
-    pd.DataFrame(examples).to_json(output_dir / "predictions.jsonl", orient="records", lines=True)
+    with open(output_dir / "metrics.json", "wt") as fout:
+        json.dump(metrics, fout)
 
 
 if __name__ == "__main__":
