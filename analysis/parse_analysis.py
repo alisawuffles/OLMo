bba30f801fbc60c38e8916d8e8990f3e31050869
Alisa Liu <alisa.46@gmail.com>
2025-02-18 15:20:43 -0800

    make it work for arbitrary ngrams

--- analysis/parse_analysis.py
+++ analysis/parse_analysis.py
@@ -1,3 +1,5 @@
+import json
+import os
 import pandas as pd
 import stanza
 from transformers import AutoTokenizer
@@ -10,6 +12,7 @@ from analysis.constituency_parsing import (
 )
 from tqdm.auto import tqdm
 
+ngram_size = 4
 
 nlp = stanza.Pipeline(lang="en", processors="tokenize,pos,constituency")
 
@@ -22,43 +25,56 @@ df = pd.read_json(
 tokenizer_path = "models/hf_models/OLMo2-7B-pts200k-t80k-ctx2756-colon/step1000"
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 
-fo = open("analysis/constituency_results.jsonl", "w")
+t = os.path.dirname(tokenizer_path).split("-")[-3]
+fo = open(f"analysis/constituency_results_{t}_{ngram_size}gram.jsonl", "w")
 
-for i, row in tqdm(df.iterrows()):
+for i, row in tqdm(df.iterrows(), total=len(df)):
     doc = nlp(row["text"])
     for sentence in doc.sentences:
         sentence_str = sentence.text
+
+        # collect token boundaries to determine whether n-grams cross token boundaries
         tokenized = tokenizer.convert_ids_to_tokens(tokenizer.encode(sentence_str))
         token_boundaries = [0]
         for token in tokenized:
             token_boundaries.append(token_boundaries[-1] + len(token))
 
+        # constituency parse
         tree_str = str(sentence.constituency)
         tokens = tokenize(tree_str)
         root = parse_constituency_tree(tokens)
         word_list = []
         collect_words(root, word_list)
         words = [word.label for word in word_list]
+        num_words = len(word_list)
+
+        if num_words != len(sentence.tokens):
+            print("Mismatched number of words and tokens")
+            print(sentence_str)
+            print(f"words ({len(words)}): {words}")
+            print(f"tokens ({len(sentence.tokens)}): {[token.text for token in sentence.tokens]}")
+            continue
 
         # collect all paths
         paths = []
-        for idx in range(len(word_list)):
+        for idx in range(num_words):
             paths.append(get_path(word_list[idx]))
 
-        # find the min level for every pair of neighboring words
+        # find the min level for the first and last word in every n-gram
         min_levels = []
         sentence_start_idx = sentence.tokens[0].start_char
-        for idx in range(len(word_list) - 1):
-            lca = find_lca(paths[idx], paths[idx + 1])
-            min_level = max(paths[idx].index(lca), paths[idx + 1].index(lca))
+        for idx in range(num_words - (ngram_size - 1)):
+            lca = find_lca(paths[idx], paths[idx + (ngram_size - 1)])
+            min_level = max(paths[idx].index(lca), paths[idx + (ngram_size - 1)].index(lca))
             a, b = (
                 sentence.tokens[idx].start_char - sentence_start_idx,
-                sentence.tokens[idx + 1].end_char - sentence_start_idx,
+                sentence.tokens[idx + (ngram_size - 1)].end_char - sentence_start_idx,
             )
             within_single_token = True
             if any([a < boundary < b for boundary in token_boundaries]):
                 within_single_token = False
-            min_levels.append((word_list[idx].label, word_list[idx + 1].label, min_level, within_single_token))
+            words = tuple([word_list[idx + i].label for i in range(ngram_size)])
+            min_levels.append((words, min_level, within_single_token))
 
         ex = {
             "sentence": sentence_str,
@@ -67,7 +83,7 @@ for i, row in tqdm(df.iterrows()):
             "min_levels": min_levels,
         }
 
-        fo.write(f"{ex}\n")
+        fo.write(json.dumps(ex) + "\n")
 
     if i % 100 == 0:
         fo.flush()
