0976d78cd8710f3f689c2f224b740323a81f58dc
Alisa Liu <alisa.46@gmail.com>
2025-03-15 11:58:14 -0700

    add loss data too

--- analysis/entropy_analysis.py
+++ analysis/entropy_analysis.py
@@ -1,10 +1,10 @@
 import json
 import os
-import random
 
 import click
 import pandas as pd
 import torch
+import torch.nn.functional as F
 from tqdm.auto import tqdm
 from transformers import AutoTokenizer
 
@@ -12,7 +12,7 @@ from eval.util import load_model_and_tokenizer
 
 
 @click.command()
-@click.option("--pt_model_path", type=str)
+@click.option("--model_path", type=str)
 @click.option("--pts_tokenizer_path", type=str)
 @click.option(
     "--eval_dir",
@@ -22,20 +22,18 @@ from eval.util import load_model_and_tokenizer
 )
 @click.option("--max_num_examples", type=int, default=1)
 @click.option("--max_context_length", type=int, default=None)
-@click.option("--constraint_beam_size", type=int, default=4)
 @click.option("--batch_size", type=int, default=1)
 @click.option("--overwrite", is_flag=True, default=False)
 def main(
-    pt_model_path: str,
+    model_path: str,
     pts_tokenizer_path: str,
     eval_dir: str,
     max_num_examples: int,
     max_context_length: int,
-    constraint_beam_size: int,
     batch_size: int,
     overwrite: bool,
 ):
-    model, pt_tokenizer = load_model_and_tokenizer(pt_model_path, padding_side="right")
+    model, tokenizer = load_model_and_tokenizer(model_path, padding_side="right")
     max_context_length = max_context_length or model.config.max_position_embeddings
 
     pts_tokenizer = AutoTokenizer.from_pretrained(pts_tokenizer_path)
@@ -50,39 +48,54 @@ def main(
             break
     print(f"Loaded {len(eval_data)} examples.")
 
-    t = os.path.dirname(pts_tokenizer_path).split("-")[-3]
-    fo_path = f"analysis/data/entropy_results_{t}.jsonl"
+    # cursed naming convention
+    model_size_str = [s for s in os.path.dirname(model_path).split("-") if s.endswith("B")][0]
+    if "pts200k" in model_path:
+        transition_point_str = [s for s in os.path.dirname(model_path).split("-") if s.startswith("t")][0]
+        t1 = f"{model_size_str}-{transition_point_str}-step8000"
+    else:
+        t1 = f"{model_size_str}-pt"
+
+    t2 = os.path.dirname(pts_tokenizer_path).split("-")[-3]  # tokenizer transition point
+    fo_path = f"analysis/data/loss_analysis_{t1}_{t2}.jsonl"
     if os.path.exists(fo_path) and not overwrite:
         print(f"File {fo_path} already exists. Skipping.")
         return
+    print(f"Results will be written to {fo_path}", flush=True)
 
     fo = open(fo_path, "w")
 
     for i in tqdm(range(0, len(eval_data), batch_size)):
         batch_texts = eval_data[i : i + batch_size]
+        inputs = tokenizer(
+            [tokenizer.eos_token + text for text in batch_texts],
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            max_length=max_context_length,
+        )
         pts_inputs = pts_tokenizer(
             [pts_tokenizer.eos_token + text for text in batch_texts],
             return_tensors="pt",
-            padding=True,
-            truncation=True,
-            max_length=max_context_length,
-        )
-        pt_inputs = pt_tokenizer(
-            [pt_tokenizer.eos_token + text for text in batch_texts],
-            return_tensors="pt",
-            padding=True,
-            truncation=True,
-            max_length=max_context_length,
+            padding="longest",
         )
         pts_input_ids = pts_inputs.input_ids
-        pt_input_ids = pt_inputs.input_ids
+        input_ids = inputs.input_ids
 
+        # entropy of output logits at every time step
         with torch.no_grad():
-            # entropy of output logits at every time step
-            outputs = model(input_ids=pt_input_ids, labels=pt_input_ids, return_dict=True)
+            outputs = model(input_ids=input_ids, labels=input_ids, return_dict=True)
             logits = outputs.logits
             probs = torch.nn.functional.softmax(logits, dim=-1)
-            entropies = -torch.sum(probs * torch.log(probs), dim=-1)
+            entropies = -torch.sum(probs * torch.log(probs), dim=-1).detach()
+            shift_logits = logits[..., :-1, :].contiguous()
+            shift_labels = input_ids[..., 1:].contiguous()
+            losses = F.cross_entropy(
+                shift_logits.view(-1, shift_logits.size(-1)),
+                shift_labels.view(-1),
+                ignore_index=tokenizer.pad_token_id,
+                reduction="none",
+            ).view(input_ids.size(0), -1)
 
         for i in range(len(batch_texts)):
             # collect token boundaries to determine whether n-grams cross token boundaries
@@ -91,17 +104,15 @@ def main(
             for token in pts_tokenized:
                 pts_token_boundaries.append(pts_token_boundaries[-1] + len(token))
 
-            pt_tokenized = pt_tokenizer.convert_ids_to_tokens(
-                pt_input_ids[i][pt_input_ids[i] != pt_tokenizer.pad_token_id]
-            )
-            pt_token_boundaries = [0]
-            for token in pt_tokenized:
-                pt_token_boundaries.append(pt_token_boundaries[-1] + len(token))
+            tokenized = tokenizer.convert_ids_to_tokens(input_ids[i][input_ids[i] != tokenizer.pad_token_id])
+            token_boundaries = [0]
+            for token in tokenized:
+                token_boundaries.append(token_boundaries[-1] + len(token))
 
             # for each token, determine whether it's contained entirely in a superword token
             data = []
-            for idx in range(1, len(pt_tokenized)):  # start at idx 1 to skip EOS token
-                a, b = pt_token_boundaries[idx], pt_token_boundaries[idx + 1]
+            for idx in range(1, len(tokenized)):  # start at idx 1 to skip EOS token
+                a, b = token_boundaries[idx], token_boundaries[idx + 1]
                 within_superword_token = False
                 if any(
                     [
@@ -121,12 +132,19 @@ def main(
                     ][0]
                     pts_token = pts_tokenized[token_idx]
 
-                data.append((pt_tokenized[idx], entropies[i][idx - 1].item(), within_superword_token, pts_token))
-            print(data)
+                data.append(
+                    {
+                        "token": tokenized[idx],
+                        "entropy": entropies[i][idx - 1].item(),
+                        "loss": losses[i][idx - 1].item(),
+                        "within_superword_token": within_superword_token,
+                        "pts_token": pts_token,
+                    }
+                )
 
             ex = {
                 "text": batch_texts[i],
-                "pt_tokens": pt_tokenized[1:],
+                "tokens": tokenized[1:],
                 "pts_tokens": pts_tokenized[1:],
                 "data": data,
             }
